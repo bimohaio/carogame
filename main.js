c6b95afc84cbe22285ec0e6c27e3d488504d39e6
thinhdh <thinhdh@d-soft.com.vn>
2025-04-24 17:29:14 +0700

    add icon for caro game

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
+var DIRECTION_VECTORS = [
+    [0, 1],
+    [1, 0],
+    [-1, -1],
+    [1, -1],
+];
 var caroBoardElement = document.getElementById('caro_app');
 var caroBoardValue;
+var caroBoardParams = [20, 20];
+var caroScore = [0, 0];
 var turn = true;
 var createMatrix = function (rows, cols, defaultValue) {
-    return Array.from({ length: rows }, function () {
-        return Array.from({ length: cols }, function () { return defaultValue; });
+    return Array.from({ length: cols }, function () {
+        return Array.from({ length: rows }, function () { return defaultValue; });
     });
 };
 var parseCellId = function (cellId) {
@@ -12,6 +20,8 @@ var parseCellId = function (cellId) {
         ? { i: parseInt(match[1], 10), j: parseInt(match[2], 10) }
         : null;
 };
+var addVector = function (location, vector) { return [location[0] + vector[0], location[1] + vector[1]]; };
+var mulWithK = function (vector, k) { return [k * vector[0], k * vector[1]]; };
 var createElement = function (tagName, classNames, objectAssign, id) {
     if (id === void 0) { id = null; }
     var element = document.createElement(tagName);
@@ -31,14 +41,51 @@ var createElement = function (tagName, classNames, objectAssign, id) {
 };
 var renderCaroBoard = function (rowNum, colNum) {
     caroBoardValue = createMatrix(rowNum, colNum, null);
-    for (var i = 0; i < rowNum; i++) {
+    caroBoardElement.innerHTML = '';
+    for (var i = 0; i < colNum; i++) {
         var rowElement = createElement('div', ["row_".concat(i), 'row_item'], null);
-        for (var j = 0; j < colNum; j++) {
+        for (var j = 0; j < rowNum; j++) {
             var cellElement = createElement('div', ["cell_item"], null, "cell_".concat(i, "-").concat(j));
             rowElement.appendChild(cellElement);
         }
         caroBoardElement.appendChild(rowElement);
     }
+};
+var updateScore = function () {
+    var scoreElement = document.getElementById('score');
+    var left = caroScore[0], right = caroScore[1];
+    scoreElement.innerHTML = "".concat(left, " - ").concat(right);
+};
+var checkResult = function (location, turn, limit) {
+    if (limit === void 0) { limit = 0; }
+    var rays = [-1, 1];
+    var cotinuousScore = 0;
+    var isVictory = false;
+    for (var _i = 0, DIRECTION_VECTORS_1 = DIRECTION_VECTORS; _i < DIRECTION_VECTORS_1.length; _i++) {
+        var key = DIRECTION_VECTORS_1[_i];
+        cotinuousScore = 1;
+        for (var _a = 0, rays_1 = rays; _a < rays_1.length; _a++) {
+            var ray = rays_1[_a];
+            for (var k = 1; k <= 5; k++) {
+                var nextVector = mulWithK(key, k * ray);
+                var _b = addVector(location, nextVector), nextX = _b[0], nextY = _b[1];
+                if (nextX < 0 || nextY < 0 || nextX >= caroBoardParams[0] || nextY >= caroBoardParams[1]) {
+                    break;
+                }
+                if (caroBoardValue[nextX][nextY] === turn) {
+                    cotinuousScore++;
+                    if (cotinuousScore >= 5) {
+                        isVictory = true;
+                        return isVictory;
+                    }
+                }
+                else {
+                    break;
+                }
+            }
+        }
+    }
+    return false;
 };
 var generateBtn = document.getElementById('generate_btn');
 generateBtn === null || generateBtn === void 0 ? void 0 : generateBtn.addEventListener('click', function () {
@@ -46,6 +93,8 @@ generateBtn === null || generateBtn === void 0 ? void 0 : generateBtn.addEventLi
     var rowInput = document.getElementById('row_num');
     var rowNum = parseInt(colInput === null || colInput === void 0 ? void 0 : colInput.value) || 10;
     var colNum = parseInt(rowInput === null || rowInput === void 0 ? void 0 : rowInput.value) || 10;
+    caroBoardParams[0] = rowNum;
+    caroBoardParams[1] = colNum;
     if (rowNum < 1 || colNum < 1) {
         alert('Set row number or column number again!');
         return;
@@ -58,13 +107,28 @@ caroBoardElement.addEventListener('click', function (event) {
     if (location) {
         var i = location.i, j = location.j;
         if (caroBoardValue[i][j] !== null) {
+            alert('Đã đánh rồi, chơi cho đúng luật nào!');
             return;
         }
         caroBoardValue[i][j] = turn;
-        turn = !turn;
         var src = turn ? './icons/circle-icon.svg' : './icons/cross-icon.svg';
+        turn = !turn;
         var turnValue = createElement('img', ['caro_icon'], null);
         turnValue.src = src;
         target.appendChild(turnValue);
+        if (checkResult([i, j], !turn)) {
+            alert("".concat(!turn ? 'Parrot' : 'Dog', " win!!!!!!"));
+            !turn ? caroScore[0]++ : caroScore[1]++;
+            updateScore();
+        }
     }
+});
+caroBoardElement.addEventListener('mousemove', function (e) {
+    var rect = caroBoardElement.getBoundingClientRect();
+    var x = e.clientX - rect.left;
+    var y = e.clientY - rect.top;
+    document.body.style.cursor = !turn ? "url('./icons/small-cross-icon.svg') 16 16, auto" : "url('./icons/small-circle-icon.svg') 16 16, auto";
+});
+caroBoardElement.addEventListener('mouseleave', function (e) {
+    document.body.style.cursor = "default";
 });
--- main.ts
+++ main.ts
@@ -3,13 +3,28 @@ interface ObjectAssigment {
     value: Object;
 }
 
+type Coordinates = [number, number];
+type CaroValue = [number, number];
+
+const DIRECTION_VECTORS: Coordinates[] = [
+    [0, 1],
+    [1, 0],
+    [-1, -1],
+    [1, -1],
+];
+
 const caroBoardElement = document.getElementById('caro_app') as HTMLElement;
+const closeBtn = document.getElementById('closeModalBtn') as HTMLElement;
+const backdrop = document.getElementById('modalBackdrop') as HTMLElement;
+
 let caroBoardValue: (boolean|null)[][];
+let caroBoardParams: CaroValue = [20, 20]; 
+let caroScore: CaroValue = [0, 0];
 let turn = true;
 
 const createMatrix = <T>(rows: number, cols: number, defaultValue: T): T[][] =>
-    Array.from({ length: rows }, () =>
-      Array.from({ length: cols }, () => defaultValue)
+    Array.from({ length: cols }, () =>
+      Array.from({ length: rows }, () => defaultValue)
     );
 
 const parseCellId = (cellId: string): { i: number; j: number } | null => {
@@ -18,6 +33,9 @@ const parseCellId = (cellId: string): { i: number; j: number } | null => {
       ? { i: parseInt(match[1], 10), j: parseInt(match[2], 10) }
       : null;
   };
+
+const addVector = (location: Coordinates, vector: Coordinates): Coordinates => [location[0] + vector[0], location[1] + vector[1]];
+const mulWithK = (vector: Coordinates, k:number): Coordinates => [k * vector[0], k * vector[1]];
 
 const createElement = (tagName: string, classNames: Array<string>|null , objectAssign: ObjectAssigment|null, id:string|null = null): HTMLElement => {
     const element: any = document.createElement(tagName);
@@ -41,14 +59,52 @@ const createElement = (tagName: string, classNames: Array<string>|null , objectA
 
 const renderCaroBoard = (rowNum: number, colNum: number): void => {
     caroBoardValue = createMatrix(rowNum, colNum, null);
-    for (let i: number = 0; i < rowNum; i++) {
+    caroBoardElement.innerHTML = '';
+
+    for (let i: number = 0; i < colNum; i++) {
         const rowElement:HTMLElement = createElement('div', [`row_${i}`, 'row_item'], null);
-        for (let j: number = 0; j < colNum; j++) {
+        for (let j: number = 0; j < rowNum; j++) {
             const cellElement:HTMLElement = createElement('div', [`cell_item`], null, `cell_${i}-${j}`);
             rowElement.appendChild(cellElement);
         }
         caroBoardElement.appendChild(rowElement);
     }
+}
+
+const updateScore = ():void => {
+    const scoreElement = document.getElementById('score') as HTMLElement;
+    const [left, right] = caroScore;
+    scoreElement.innerHTML = `${left} - ${right}`;
+}
+
+const checkResult = (location: Coordinates, turn: boolean, limit: number = 0) => {
+    const rays: Coordinates = [-1, 1];
+    let cotinuousScore: number = 0;
+    let isVictory: boolean = false;
+
+    for (let key of DIRECTION_VECTORS) {
+        cotinuousScore = 1;
+        for (let ray of rays) {
+            for (let k: number = 1; k <= 5; k++) {
+                const nextVector: Coordinates = mulWithK(key, k * ray);
+                const [nextX, nextY]: Coordinates = addVector(location, nextVector);
+                if (nextX < 0 || nextY < 0 || nextX >= caroBoardParams[0] || nextY >= caroBoardParams[1]) {
+                    break;
+                }
+
+                if (caroBoardValue[nextX][nextY] === turn) {
+                    cotinuousScore++;
+                    if (cotinuousScore >= 5) {
+                        isVictory = true;
+                        return isVictory;
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+    return false;
 }
 
 const generateBtn: HTMLElement|null = document.getElementById('generate_btn');
@@ -59,6 +115,8 @@ generateBtn?.addEventListener('click', function () {
 
     const rowNum: number = parseInt(colInput?.value) || 10;
     const colNum: number = parseInt(rowInput?.value) || 10;
+    caroBoardParams[0] = rowNum;
+    caroBoardParams[1] = colNum;
 
     if (rowNum < 1 || colNum < 1) {
         alert('Set row number or column number again!');
@@ -73,15 +131,44 @@ caroBoardElement.addEventListener('click', (event) => {
     if (location) {
         const {i, j} = location;
         if (caroBoardValue[i][j] !== null) {
+            alert('Đã đánh rồi, chơi cho đúng luật nào!');
             return;
         }
         caroBoardValue[i][j] = turn;
-        turn = !turn
         const src:string = turn ? './icons/circle-icon.svg' : './icons/cross-icon.svg';
         const turnValue: any = createElement('img', ['caro_icon'], null);
         turnValue.src = src;
         target.appendChild(turnValue);
+        if (checkResult([i, j], turn)) {
+            alert(`${turn ? 'Parrot' : 'Dog' } win!!!!!!`);
+            backdrop.style.display = 'flex';
+            turn ? caroScore[0]++ : caroScore[1]++;
+            updateScore();
+        }
+        turn = !turn
     }
 });
 
+caroBoardElement.addEventListener('mousemove', function(e) {
+    const rect = caroBoardElement.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    document.body.style.cursor = !turn ? "url('./icons/small-cross-icon.svg') 16 16, auto" : "url('./icons/small-circle-icon.svg') 16 16, auto";
+  });
+
+caroBoardElement.addEventListener('mouseleave', function(e) {
+    document.body.style.cursor = "default";
+  });
+
+  
+
+closeBtn.addEventListener('click', () => {
+    backdrop.style.display = 'none';
+});
+
+backdrop.addEventListener('click', (e) => {
+    if (e.target === backdrop) {
+        backdrop.style.display = 'none';
+    }
+});
     
